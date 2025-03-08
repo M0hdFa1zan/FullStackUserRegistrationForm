e8dfad91cd9ea747141b10b4985f2a59eb0c7971
M0hdFa1zan <140683928+M0hdFa1zan@users.noreply.github.com>
2025-03-08 12:13:50 +0530

    Update userApi.js

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://userregistrationform-9rrr.onrender.com';
+const API_URL = 'https://userregistrationform-9rrr.onrender.com/users';
 
 export const fetchUsers = async () => await axios.get(API_URL).then(res => res.data);
 export const addUser = async (userData) => await axios.post(API_URL, userData).then(res => res.data);
